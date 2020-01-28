ae6c0d9765d4e1de7488ed082f8e1babd0a8c7d6
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2020-01-28 17:14:24 +0530

    Fixed Event Route

--- routes/Events/Events.js
+++ routes/Events/Events.js
@@ -4,10 +4,14 @@ const getEvents = require('./GetEvents');
 const postEvents = require('./AddEvents');
 const updateEvents = require('./UpdateEvent');
 const deleteEvents = require('./DeleteEvent');
+const verifyToken = require('../../middlewares/verifyToken');
+const adminAccess = require('../../middlewares/adminAccess');
 
 
-router.use('/', [getEvents, updateEvents, deleteEvents]);
-router.use('/add', postEvents);
+
+router.use('/', getEvents);
+router.use('/', [verifyToken, adminAccess], [updateEvents, deleteEvents])
+router.use('/add', [verifyToken, adminAccess], postEvents);
 
 
 module.exports = router;
